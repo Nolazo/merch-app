961207067ab6f831581445144d58abd0b639ec60
OMEN <j.manuelvaldebenito@gmail.com>
2022-07-12 00:46:37 -0400

    Incluidos los estilos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOMClient from 'react-dom/client'
-import App from './components/App'
+import App from './routes/App'
 
 const container = document.getElementById('root')
 const root = ReactDOMClient.createRoot(container)
